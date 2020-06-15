242a1225610752632fc273679038037e50cb4a43
ericsmith9161 <ericsmith9161@gmail.com>
2020-06-15 18:22:25 -0400

    Complete splash div shell

--- frontend/src/components/Splash.jsx
+++ frontend/src/components/Splash.jsx
@@ -26,7 +26,93 @@ const Splash = () => (
 
       </section>
       <section className="splash-searchbar">
+        <div className="splash-searchbar-searchbar">
 
+        </div>
+        <div className="splash-searchbar-gov-lvl">
+
+        </div>
+        <div className="splash-searchbar-interests">
+
+        </div>
+        <div className="splash-searchbar-search-btn">
+
+        </div>
+      </section>
+    </section>
+    
+    <section className="splash-body">
+      <section className="splash-body-one">
+        <div className="splash-body-one-one">
+
+        </div>
+        <div className="splash-body-one-two">
+
+        </div>
+        <div className="splash-body-one-three">
+
+        </div>
+      </section>
+
+      <section className="splash-body-two">
+        <div className="splash-body-politician-show">
+
+        </div>
+        <div className="splash-body-politician-show-explanation">
+
+        </div>
+      </section>
+
+      <section className="splash-body-three">
+        <div className="splash-about-and-why">
+
+        </div>
+        <div className="splash-about-and-why-data">
+          <div className="splash-about-and-why-datum">
+
+          </div>
+          <div className="splash-about-and-why-datum">
+
+          </div>
+          <div className="splash-about-and-why-datum">
+
+          </div>
+        </div>
+      </section>
+
+      <section className="splash-source-information">
+        <div className="open-source-header">
+
+        </div>
+        <div className="github-repo-info">
+
+        </div>
+        <div className="api-boxes">
+          <div className="api-box">
+
+          </div>
+          <div className="api-box">
+
+          </div>
+          <div className="api-box">
+
+          </div>
+        </div>
+      </section>
+
+      <section className="splash-body-bottom">
+        <div className="logo">
+
+        </div>
+        <div className="splash-body-bottom-links">
+
+        </div>
+        <div className="splash-body-bottom-links">
+
+        </div>
+        <div className="splash-body-bottom-links">
+
+        </div>
       </section>
     </section>
   </div>
