620347259edf2ea5fa63b9f35f3aa302cf7d517d
ericsmith9161 <ericsmith9161@gmail.com>
2020-06-16 19:03:23 -0400

    Search page component build out cont

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PoliticianPreview from './PoliticianPreview.jsx';
+import PoliticianPreview from './PoliticianPreview';
 import {Link} from 'react-router-dom';
 
 class Search extends React.Component{
@@ -11,46 +11,75 @@ class Search extends React.Component{
     }
   }
 
-  componentDidMount(){
-    this.props.fetchPolicians()
-  }
+  // componentDidMount(){
+  //   this.props.fetchPolicians();
+  // }
+
+  // componentDidUpdate(){
+  //   this.props.fetchPolicians();
+  // }
+
+  //component did mount/update should ultimately have a fetchPoliticians call
+  //that takes in location/interest information
 
   handleInterest(interest){
-    if(this.state.interests.includes(interest)){
-      this.setState(interests)
-    }
-    else{
-      this.setState(govLevels)
+    return (e) => {
+      let interests = [...this.state.interests] //make a separate copy
+      let index = interests.indexOf(interest)
+      if (index !== -1) {
+        interests.splice(index, 1);
+        this.setState({ interests: interests })
+      } else {
+        this.setState({ interests: interests.push(interest) })
+      }
     }
   } 
   
   handleScope(scope){
 
-  }
+    return (e) => {
+      let govLevels = [...this.state.govLevels]
+      let index = govLevels.indexOf(scope)
+      if (index !== -1) {
+        govLevels.splice(index, 1);
+        this.setState({ govLevels: govLevels })
+      } else {
+        this.setState({ govLevels: govLevels.push(scope) })
+      }
+    }
+
+  } 
 
   render(){
     // get unordered lists of local, county, district, state, fereral here?
     // something like...
-    const officialsList = this.state.govLevels.map((govLevel) => (
-      <div className="gov-level">
-        <div className="gov-level-divider">
-          {govLevel}
-        </div>
-        <ul className="gov-level-officials-list">
-          {this.props.politicians[govLevel].map((politician, idx) => {
-            <li className="official-preview" key={idx}>
-              <Link to={`/politicians/${politician.id}`}><PoliticianPreview politician={politician} /></Link>
-            </li>
-          })}
-        </ul>
-      </div>
-    ));
+    // const officialsList = this.state.govLevels.map((govLevel) => (
+    //   <div className="gov-level">
+    //     <div className="gov-level-divider">
+    //       {govLevel}
+    //     </div>
+    //     <ul className="gov-level-officials-list">
+    //       {this.props.politicians[govLevel].map((politician, idx) => {
+    //         <li className="official-preview" key={idx}>
+    //           <Link to={`/politicians/${politician.id}`}><PoliticianPreview politician={politician} /></Link>
+    //         </li>
+    //       })}
+    //     </ul>
+    //   </div>
+    // ));
 
     return(
       <div className="search-page">
         <div className="search-page-sidebar">
           <div className="search-page-sidebar-head">
+            <div className="search-page-sidebar-head-left">
+              <div className="search-page-sidebar-head-left-county"></div>
+              <div className="search-page-sidebar-head-left-district"></div>
+            </div>
+            <div className="search-page-sidebar-head-right">
+              <div className="order-by"></div>
 
+            </div>
           </div>
           <div className="search-page-sidebar-district-info">
             <div className="search-page-sidebar-leaning">
@@ -75,11 +104,39 @@ class Search extends React.Component{
         </div>
         <div className="search-page-main">
           <div className = "officials-list">
-            {officialsList}
+            {/* {officialsList} */}
+            <div className="gov-level">
+              <div className="gov-level-divider">
+                Local
+              </div>
+              <ul className="gov-level-officials-list">
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+            <div className="gov-level">
+              <div className="gov-level-divider">
+                State
+              </div>
+              <ul className="gov-level-officials-list">
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+            <div className="gov-level">
+              <div className="gov-level-divider">
+                Federal
+              </div>
+              <ul className="gov-level-officials-list">
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
           </div>
         </div>
-
       </div>
     )
   }
 }
+
+export default Search;
--- frontend/src/components/app/app.js
+++ frontend/src/components/app/app.js
@@ -2,12 +2,15 @@ import React from "react";
 
 import { AuthRoute } from "../../util/route_util";
 import Session from "../session/session";
+import Search from "../Search";
 
 function App() {
   return (
     <div className="App">
    authComponents
       <AuthRoute exact path={["/register", "/login"]} component={Session} />
+      <AuthRoute path={"/"} component={Search} />
+
     </div>
   );
 }
