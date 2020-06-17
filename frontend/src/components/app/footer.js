7aa550610bda7d3622ea4020a7ffb47e9ae1629e
Megan <megan.mcnulty07@gmail.com>
2020-06-17 12:27:22 -0400

    Footer styling

--- frontend/src/components/app/footer.js
+++ frontend/src/components/app/footer.js
@@ -1,5 +1,20 @@
-const Footer = props => (
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Footer = () => (
     <footer className="main-footer">
-        <button></button>
+        <button className="github">
+            <FontAwesomeIcon icon={["fab", "github"]} />
+            <p>github</p>
+        </button>
+
+        <button>
+            REGISTER TO VOTE
+        </button>
+        <button>
+            ABOUT US
+        </button>
     </footer>
 )
+
+export default Footer;
