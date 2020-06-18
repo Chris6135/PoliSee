3ac0920d3d07be7359f2572068dcd4737e64da24
Megan <megan.mcnulty07@gmail.com>
2020-06-18 02:04:44 -0400

    Refactored selects into dropdowns

---
+++ frontend/src/components/search/search_bar_.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+
+import { fetchRepresentatives } from "../../actions/search_actions";
+
+const SearchBar = ({ history }) => {
+  const [address, setAddress] = useState("");
+  const [levels, setLevels] = useState([]);
+  const [issues, setIssues] = useState([]);
+  const [error, setError] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (address.trim().length) {
+      const lvls = levels.join("%20").replace(/\s/, "%20") || "all";
+      const issu = issues.join("%20") || "all";
+      return dispatch(fetchRepresentatives(address)).then(
+        history.push(`/results?levels=${lvls}&issues=${issu}`)
+      );
+    } else {
+      setError(true);
+    }
+  };
+
+  const handleChange = (e) => setAddress(e.target.value);
+
+  const handleOptions = (type) => (e) => {
+    return type === "levels"
+      ? setLevels(levels.concat(e.target.value))
+      : setIssues(issues.concat(e.target.value));
+  };
+
+  return (
+    <form className="search-form" onSubmit={handleSubmit}>
+      <div className="search-wrapper">
+        <input
+          type="text"
+          className="address-input"
+          value={address}
+          onChange={handleChange}
+          placeholder="ENTER YOUR ADDRESS TO BEGIN"
+        />
+        <li value="test-value" onClick={ e => console.log(e.target.getAttribute("value")) }> Test </li>
+
+        <div className="options-container">
+          <div className="select-container" >
+            <select
+              defaultValue={levels}
+              name="level"
+              size="4"
+              multiple
+              onChange={handleOptions("levels")}
+            > 
+              <option value="all">ALL LEVELS</option>
+              <option value="country">FEDERAL</option>
+              <option value="administrativeArea1">STATE</option>
+              <option value="administrativeArea2">COUNTY</option>
+              <option value="locality subLocality1 subLocality2">LOCAL</option>
+            </select>
+            </div>
+            <div className="select-container" >
+            <select
+              defaultValue={issues}
+              name="issue"
+              size="3"
+              multiple
+              onChange={handleOptions("issues")}
+            >
+              <option value="all">ISSUES</option>
+              <option value="justice">JUSTICE</option>
+              <option value="education">EDUCATION</option>
+              <option value="legislation">LEGISLATION</option>
+            </select>
+          </div>
+        </div>
+        <button type="submit">GO</button>
+      </div>
+      {error && <span className="err-msg">Please enter an address</span>}
+    </form>
+  );
+};
+
+export default SearchBar;
